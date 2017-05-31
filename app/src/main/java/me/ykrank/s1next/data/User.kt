ba1d6da49aa32f1ad7361fca8ca0b4e2edd2d581
ykrank <yuanke@sencent.com>
2017-05-31 14:59:41 +0800

    Use kotlin

---
+++ app/src/main/java/me/ykrank/s1next/data/User.kt
@@ -0,0 +1,29 @@
+package me.ykrank.s1next.data
+
+import android.text.TextUtils
+
+open class User {
+
+    @Volatile var uid: String? = null
+
+    @Volatile var name: String? = null
+
+    @Volatile var permission: Int = 0
+
+    @Volatile var authenticityToken: String? = null
+
+    @Volatile var appSecureToken: String? = null
+
+    @Volatile open var isLogged: Boolean = false
+
+    val isAppLogged: Boolean
+        get() = !TextUtils.isEmpty(appSecureToken)
+
+    val key: String
+        get() {
+            if (!TextUtils.isEmpty(uid)) {
+                return uid!!
+            }
+            return "anonymous"
+        }
+}
